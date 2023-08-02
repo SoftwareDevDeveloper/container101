db3e22f35f59e378903d9d2744870c5b06158310
Rufus Ajiboye <ruufman@gmail.com>
2023-08-02 12:03:05 +0100

    Updated code to use redis

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
 regions = {"uk":"London","nigeria":"Lagos","southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
-version = "1.7"
+version = "1.6"
+
+r = redis.Redis(host="cache-server", port=6379)
 
 
 @app.route("/getregion/<country>")
 def lookup(country):
     value ="N/A"
     try:
-        value = r.get(country)
+        value = r.get (country)
     except:
         pass
+    return value
+
+
+@app.route("/saveregion/<country>/<city>")
+def savedata(country, city):
+    value ="PASS"
+    try:
+        r.set(country,city)
+    except:
+        value = "FAIL"
     return value 
 
 
