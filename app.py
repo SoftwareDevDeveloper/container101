a46f01962ebd94230a6436ef08b63af2d6d7af85
Rufus Ajiboye <ruufman@gmail.com>
2023-08-02 11:49:15 +0100

    File Updated

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
-import redis
 
 app = Flask(__name__)
-# regions = {"uk":"London","nigeria":"Lagos","southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
-version = "1.7.1"
-
-r = redis.Redis(host="cache-server", port=6379)
-
+regions = {"uk":"London","nigeria":"Lagos","southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
+version = "1.7"
 
 
 @app.route("/getregion/<country>")
@@ -17,16 +13,6 @@ def lookup(country):
     except:
         pass
     return value 
-
-
-@app.route("/saveregion/<country>/<city>")
-def savedata(country, city):
-    value ="pass"
-    try:
-        r.set(country, city)
-    except:
-        value = "fail"
-    return value
 
 
 @app.route("/version")
