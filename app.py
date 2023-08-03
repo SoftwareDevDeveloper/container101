4fa4e2f3182884fef979f6b5efd7cc0160d090a7
Rufus Ajiboye <ruufman@gmail.com>
2023-08-03 18:37:02 +0100

    Index file updated with html code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import redis
 
 app = Flask(__name__)
@@ -8,24 +8,34 @@ version = "1.7.2"
 r = redis.Redis(host="cache-server", port=6379)
 
 
-@app.route("/getregion/<country>")
-def lookup(country):
-    value ="N/A"
+@app.route("/getregion", methods = ["GET"])
+def getregion():
+    value = '<h1> Not found</h1>'
+    country = request.args.get("country")
+
     try:
-        value = r.get (country)
+        city = r.get (country)
+        value = '''<h1> {} is in {} </h1>'''.format(city, country)
     except:
         pass
+
     return value
 
 
-@app.route("/saveregion/<country>/<city>")
-def savedata(country, city):
-    value ="PASS"
+@app.route("/saveregion", methods = ["GET"])
+def saveregion():
+    country = request.args.get("country")
+    city = request.args.get("city")
+
+    value = "<h1> failed to save </h1>"
+
     try:
-        r.set(country,city)
+        r.set(country, city)
+        value = '''<h1> {} is in {} saved successfully </h1>'''.format(city, country)
     except:
-        value = "FAIL"
-    return value 
+        pass
+        
+    return value
 
 
 @app.route("/version")
