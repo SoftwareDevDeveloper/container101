827a7b487cb6d1948550863e1af6a5960319bbf9
Rufus Ajiboye <ruufman@gmail.com>
2023-08-07 12:23:58 +0100

    Files Updated to default

--- app.py
+++ app.py
@@ -13,30 +13,6 @@ def getregion():
     value = '<h1> Not found</h1>'
     country = request.args.get("country")
 
-    try:
-        city = r.get (country)
-        value = '''<h1> {} is in {} </h1><p><a href= "http://54.228.33.230">home</a>'''.format(city.upper(), country.upper())
-    except:
-        pass
-
-    return value
-
-
-@app.route("/saveregion", methods = ["GET"])
-def saveregion():
-    value = "<h1> failed to save </h1>"
-
-    country = request.args.get("country")
-    city = request.args.get("city")
-
-    try:
-        r.set(country, city)
-        value = '''<h1> {} is in {} saved successfully </h1>'''.format(city.upper(), country.upper())
-    except:
-        pass
-        
-    return value
-
 
 @app.route("/version")
 def getversion():
