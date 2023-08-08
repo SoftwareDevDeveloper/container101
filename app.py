c6dbff365c13fe90e7e6be8279080facb7203bc4
Rufus Ajiboye <ruufman@gmail.com>
2023-08-08 21:23:44 +0100

    File Updated

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
-from flask import Flask
+
+from flask import Flask, request
 import redis
 
 app = Flask(__name__)
-#   regions = {"uk":"London", "nigeria":"Lagos", "southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
 version = "1.7.1"
 
 r = redis.Redis(host="cache-server", port=6379)
 
-
-@app.route("/getregion/<country>")
-def lookup(country):
-    value = "pass"
+@app.route("/getregion", methods = ["GET"])
+def getregion():
+    value ='<h1>Not found</h1>'
+    country = request.args.get("country")
     try:
-        value = r.get (country)
+        city = r.get (country)
+        value = '''<h1> {} is in {} </h1> <p><a href= "http://34.245.126.27">home</a>'''.format(city.upper(), country.upper())
     except:
-        value = "fail"
-    return value
-
-
-app.route("/saveregion/<country>/<city>")
-def savedata(country, city):
-    value = "pass"
-    try:
-        r.set(country,city)
-    except:
-        value = "fail"
-    return value
-
+        pass
+    return  value
 
 @app.route("/version")
 def getversion():
     return version
 
+@app.route("/saveregion", methods = ["GET"])
+def saveregion():
+    value = "<h1> Failed to save</h1>"
+
+    country = request.args.get("country")
+    city = request.args.get("city")
+ 
+    try:
+        r.set(country,city)
+        value = '''<h1> {} is in {} saved successfully </h1><p><a href= "http://34.245.126.27">home</a>'''.format(city.upper(), country.upper())
+    except:
+        pass
+    return  value
 
 
 
