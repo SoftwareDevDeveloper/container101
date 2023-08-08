7e09ae5c5bc709e6ee3e59ae743c6efefc682be9
Rufus Ajiboye <ruufman@gmail.com>
2023-08-08 13:35:21 +0100

    Updated code to use redis

--- app.py
+++ app.py
@@ -1,79 +1,78 @@
-# from flask import Flask
-# import redis
-
-# app = Flask(__name__)
-# regions = {"uk":"London", "nigeria":"Lagos", "southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
-# version = "1.6"
-
-# r = redis.Redis(host="cache-server", port=6379)
-
-
-# @app.route("/getregion/<country>")
-# def lookup(country):
-#     value = "N/A"
-#     try:
-#         value = r.get (country)
-#     except:
-#         pass
-#     return value
-
-
-# app.route("/saveregion/<country>/<city>")
-# def savedata(country, city):
-#     value = "pass"
-#     try:
-#         r.set(country, city)
-#     except:
-#         value = "fail"
-#     return value
-
-
-# @app.route("/version")
-# def getversion():
-#     return version
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from flask import Flask
+import redis
 
 app = Flask(__name__)
-regions = {"uk":"London", "nigeria":"Lagos", "southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
+# regions = {"uk":"London", "nigeria":"Lagos", "southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
 version = "1.6"
+
+r = redis.Redis(host="cache-server", port=6379)
 
 
 @app.route("/getregion/<country>")
 def lookup(country):
     value = "N/A"
     try:
-        value = regions [country.lower()]
+        value = r.get (country)
     except:
         pass
+    return value
+
+
+app.route("/saveregion/<country>/<city>")
+def savedata(country, city):
+    value = "pass"
+    try:
+        r.set(country, city)
+    except:
+        value = "fail"
     return value
 
 
 @app.route("/version")
 def getversion():
     return version
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+# regions = {"uk":"London", "nigeria":"Lagos", "southafrica":"Johannesburg", "usa":"DC", "us":"DC","ghana":"accra", "togo":"lome"}
+# version = "1.6"
+
+
+# @app.route("/getregion/<country>")
+# def lookup(country):
+#     value = "N/A"
+#     try:
+#         value = regions [country.lower()]
+#     except:
+#         pass
+#     return value
+
+
+# @app.route("/version")
+# def getversion():
+#     return version
